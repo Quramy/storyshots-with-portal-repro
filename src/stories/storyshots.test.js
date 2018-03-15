7fe5c7391fb0cc20fcfd5ea8d418b30ec8232c51
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-03-15 11:42:48 +0900

    modify

--- src/stories/storyshots.test.js
+++ src/stories/storyshots.test.js
@@ -1,9 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
-// global.window = global;
-// window.addEventListener = () => {};
-// window.requestAnimationFrame = () => {
-//   throw new Error('requestAnimationFrame is not supported in Node');
-// };
+// Workaround for https://github.com/facebook/react/issues/11565#issuecomment-368877149
+const ReactDOM = require('react-dom');
+const React = require('React');
+ReactDOM.createPortal = node => React.createElement(node, null);
 
 initStoryshots();
